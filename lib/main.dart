6e0d9c41ab691c80dfe509b7429eafdaa2b1e12e
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-01-17 19:48:05 +0700

    fix: Hot reload
    Move router & apptheme as parameter

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:backtix_app/src/app.dart';
 import 'package:backtix_app/src/blocs/app_bloc_observer.dart';
 import 'package:backtix_app/src/blocs/auth/auth_bloc.dart';
 import 'package:backtix_app/src/blocs/theme_mode/theme_mode_cubit.dart';
+import 'package:backtix_app/src/config/app_theme.dart';
+import 'package:backtix_app/src/config/routes/app_route.dart';
 import 'package:backtix_app/src/service_locator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -41,6 +43,9 @@ Future<void> main() async {
         await windowManager.setAlignment(Alignment.topRight);
       }
 
+      final router = AppRoute().goRouter;
+      final appTheme = AppTheme();
+
       runApp(
         MultiBlocProvider(
           providers: [
@@ -51,7 +56,7 @@ Future<void> main() async {
               create: (_) => GetIt.I<AuthBloc>(),
             ),
           ],
-          child: const App(),
+          child: App(router: router, appTheme: appTheme),
         ),
       );
     },
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,16 @@
 import 'package:backtix_app/src/blocs/theme_mode/theme_mode_cubit.dart';
 import 'package:backtix_app/src/config/app_theme.dart';
-import 'package:backtix_app/src/config/routes/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key, required this.router, required this.appTheme});
+
+  final RouterConfig<Object> router;
+  final AppTheme appTheme;
 
   @override
   Widget build(BuildContext context) {
-    final router = AppRoute().goRouter;
-    final appTheme = AppTheme();
-
     return BlocBuilder<ThemeModeCubit, ThemeMode>(
       builder: (_, themeMode) {
         return MaterialApp.router(
