656ea020b4fcd106d98a4bb793c8a5403471a239
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-01-21 09:02:00 +0700

    chore: Add shimmer

---
+++ lib/src/presentations/widgets/other/shimmer.dart
@@ -0,0 +1,25 @@
+import 'package:backtix_app/src/core/extensions/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart' as s;
+
+class Shimmer extends StatelessWidget {
+  const Shimmer({super.key, this.child});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = context.isDark;
+
+    return s.Shimmer.fromColors(
+      baseColor: Colors.grey[isDark ? 800 : 400]!,
+      highlightColor: Colors.grey[isDark ? 700 : 300]!,
+      child: child ??
+          Container(
+            color: Colors.grey[500]!,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+    );
+  }
+}
--- lib/src/presentations/widgets/widgets.dart
+++ lib/src/presentations/widgets/widgets.dart
@@ -3,3 +3,4 @@ export 'dialog/error_dialog.dart';
 export 'form/custom_text_form_field.dart';
 export 'navigation/home_navigation_bar.dart';
 export 'navigation/home_navigation_rail.dart';
+export 'other/shimmer.dart';
