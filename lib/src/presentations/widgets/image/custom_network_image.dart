cb6756d30587449a904cbfad1181f3b6206e2fe1
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-01-31 10:43:20 +0700

    chore: Update custom network image
    - Add cached network image option
    - Add easy image viewer

--- lib/src/presentations/widgets/image/custom_network_image.dart
+++ lib/src/presentations/widgets/image/custom_network_image.dart
@@ -1,5 +1,6 @@
 import 'package:backtix_app/src/core/extensions/extensions.dart';
 import 'package:backtix_app/src/presentations/widgets/widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomNetworkImage extends StatelessWidget {
@@ -7,16 +8,22 @@ class CustomNetworkImage extends StatelessWidget {
     super.key,
     required this.src,
     this.small = false,
+    this.fit = BoxFit.cover,
+    this.cached = true,
   });
 
   final String src;
   final bool small;
+  final BoxFit fit;
+  final bool cached;
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      src,
-      fit: BoxFit.cover,
+    return Image(
+      image: cached
+          ? CachedNetworkImageProvider(src)
+          : NetworkImage(src) as ImageProvider,
+      fit: fit,
       loadingBuilder: (_, child, loadingProgress) {
         if (loadingProgress != null) return const Shimmer();
         return child;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import flutter_inappwebview_macos
 import google_sign_in_ios
 import path_provider_foundation
 import screen_retriever
+import sqflite
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
