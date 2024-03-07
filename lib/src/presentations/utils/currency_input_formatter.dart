effdd4364d84e335a3188b49406395c6ffb6cab6
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-03-07 21:40:52 +0700

    Update currency_input_formatter.dart
    Improve UX

--- lib/src/presentations/utils/currency_input_formatter.dart
+++ lib/src/presentations/utils/currency_input_formatter.dart
@@ -1,4 +1,5 @@
 import 'package:backtix_app/src/config/constant.dart';
+import 'package:backtix_app/src/presentations/utils/utils.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
@@ -16,11 +17,28 @@ class CurrencyInputFormatter extends TextInputFormatter {
   ) {
     if (newValue.selection.baseOffset == 0) return newValue;
 
-    String newText = formatter.format(double.parse(newValue.text));
+    final double unformatted = Utils.unformatCurrency(newValue.text);
+    final String newText = formatter.format(unformatted);
 
+    final int unformattedStrLen = unformatted.toInt().toString().length;
+    final int oldUnformattedStrLen =
+        Utils.unformatCurrency(oldValue.text).toInt().toString().length;
+    final bool isDeleting = oldUnformattedStrLen > unformattedStrLen;
+
+    final int offset = newValue.selection.extent.offset;
+    int newOffset = offset;
+
+    if (isDeleting && unformattedStrLen % 3 == 0) {
+      newOffset--;
+    } else if (!isDeleting &&
+        (unformattedStrLen > 1 && (unformattedStrLen - 1) % 3 == 0)) {
+      newOffset++;
+    }
+
+    if (newOffset > newText.length) newOffset = newText.length;
     return newValue.copyWith(
       text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
+      selection: TextSelection.collapsed(offset: newOffset),
     );
   }
 }
