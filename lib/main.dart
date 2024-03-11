f316fb69fdc412151af6ee56eb547ff9d0b0442b
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-03-11 12:29:35 +0700

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,10 @@ import 'dart:io';
 
 import 'package:backtix_app/src/app.dart';
 import 'package:backtix_app/src/blocs/app_bloc_observer.dart';
-import 'package:backtix_app/src/blocs/auth/auth_bloc.dart';
-import 'package:backtix_app/src/blocs/onboarding/onboarding_cubit.dart';
-import 'package:backtix_app/src/blocs/theme_mode/theme_mode_cubit.dart';
-import 'package:backtix_app/src/config/app_theme.dart';
-import 'package:backtix_app/src/config/routes/app_route.dart';
 import 'package:backtix_app/src/service_locator.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:window_manager/window_manager.dart';
@@ -45,21 +38,7 @@ Future<void> main() async {
         await windowManager.setAlignment(Alignment.topRight);
       }
 
-      final authBloc = GetIt.I<AuthBloc>();
-
-      final router = AppRoute(authBloc: authBloc).goRouter;
-      final appTheme = AppTheme();
-
-      runApp(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider.value(value: authBloc),
-            BlocProvider(create: (_) => GetIt.I<ThemeModeCubit>()),
-            BlocProvider(create: (_) => GetIt.I<OnboardingCubit>()),
-          ],
-          child: App(router: router, appTheme: appTheme),
-        ),
-      );
+      App.run();
     },
     (error, stack) => log(error.toString(), stackTrace: stack),
   );
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,36 @@
+import 'package:backtix_app/src/blocs/auth/auth_bloc.dart';
+import 'package:backtix_app/src/blocs/onboarding/onboarding_cubit.dart';
 import 'package:backtix_app/src/blocs/theme_mode/theme_mode_cubit.dart';
 import 'package:backtix_app/src/config/app_theme.dart';
 import 'package:backtix_app/src/config/constant.dart';
+import 'package:backtix_app/src/config/routes/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class App extends StatelessWidget {
   const App({super.key, required this.router, required this.appTheme});
 
   final RouterConfig<Object> router;
   final AppTheme appTheme;
+
+  static void run() {
+    final authBloc = GetIt.I<AuthBloc>();
+
+    final router = AppRoute(authBloc: authBloc).goRouter;
+    final appTheme = AppTheme();
+
+    return runApp(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider.value(value: authBloc),
+          BlocProvider(create: (_) => GetIt.I<ThemeModeCubit>()),
+          BlocProvider(create: (_) => GetIt.I<OnboardingCubit>()),
+        ],
+        child: App(router: router, appTheme: appTheme),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
