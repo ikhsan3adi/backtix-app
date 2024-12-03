49f2c8b52dd988c3926d20e1986ab48f70db9d36
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-12-03 09:29:53 +0700

    Update dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,7 @@ import package_info_plus
 import path_provider_foundation
 import screen_retriever
 import share_plus
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import window_manager
 
